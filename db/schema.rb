3c2040a718b252dc597097c70c3070b745a5f89f
David Stinnette <dastinnette@gmail.com>
2015-09-22 15:03:43 -0600

    Adds item belongs to category and category show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922201416) do
+ActiveRecord::Schema.define(version: 20150922205422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150922201416) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "category_id"
   end
 
 end
