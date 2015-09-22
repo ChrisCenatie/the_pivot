ee900b7985d736c62f8911889e32f60b537ec644
David Stinnette <dastinnette@gmail.com>
2015-09-22 11:29:35 -0600

    Add full suite of user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, presence: true, uniqueness: true,
+    length: { in: 6..30 },
+    format: { with:
+      /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9._%+-]+.[a-zA-Z0-9._%+-]+\z/ }
 end
