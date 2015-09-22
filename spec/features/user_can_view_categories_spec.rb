eaea4c454564bb45dc3c2f2a94365e3c769458ad
philmphoenix <jpholzmann@gmail.com>
2015-09-22 15:48:25 -0600

    Fixed styling issues

--- spec/features/user_can_view_categories_spec.rb
+++ spec/features/user_can_view_categories_spec.rb
@@ -1,17 +1,20 @@
 require "rails_helper"
 
-RSpec.feature "use can view categories" do
+RSpec.feature "user can view categories" do
 
   it "can view categories as a guest" do
     visit "/items"
-    click_link "View by category"
+    click_link("View by category")
     expect(current_path).to eq(categories_path)
     expect(page).to have_content("Meals")
     expect(page).to have_content("Drinks")
     expect(page).to have_content("Sides")
 
     click_link("Meals")
-    expect(current_path).to eq(category_path(1))
+
+    category = Category.find_by(name: "Meals")
+
+    expect(current_path).to eq(category_path(category.id))
     expect(page).to have_content("Burger")
     expect(page).to have_content("Steak")
     expect(page).to have_content("For a rabbi")
