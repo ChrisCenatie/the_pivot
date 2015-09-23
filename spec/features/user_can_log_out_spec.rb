0fdd599b042fa7c4803539e70307cd2be0fa2963
David Stinnette <dastinnette@gmail.com>
2015-09-23 10:56:02 -0600

    Remove logout link and show login/sign up when user logs out

--- spec/features/user_can_log_out_spec.rb
+++ spec/features/user_can_log_out_spec.rb
@@ -14,6 +14,7 @@ RSpec.feature "user logs out" do
     click_link("Logout")
     expect(current_path).to eq(root_path)
     expect(page).to have_content("You are now logged out")
+    expect(page).to_not have_content("Logout")
   end
 
   scenario "when not logged in" do
