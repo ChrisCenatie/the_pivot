cb33c15f53345fb56bc309f7b692895b3deb1def
Ryan Asensio <Ryan.asensio@gmail.com>
2015-09-22 21:16:00 -0600

    Update user_can_only_checkout_while_logged_in_spec.rb

--- spec/features/user_can_only_checkout_while_logged_in_spec.rb
+++ spec/features/user_can_only_checkout_while_logged_in_spec.rb
@@ -3,11 +3,9 @@ require "rails_helper"
 RSpec.feature "user can only checkout while logged in" do
   it "directs user to login page from cart checkout" do
     visit items_path
-
     click_on("Add Fries")
-
+    
     visit cart_path
-
     click_on("Check Out")
 
     expect(current_path).to eq(login_path)
