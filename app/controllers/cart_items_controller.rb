0dd00537424c5b37fbc393e0c88c56f298a46e42
philmphoenix <jpholzmann@gmail.com>
2015-09-22 20:09:41 -0600

    Adds ability to remove items from cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,6 +5,12 @@ class CartItemsController < ApplicationController
     redirect_to cart_path
   end
 
+  def destroy
+    cart.remove_item(params[:item_id])
+    session[:cart] = cart.data
+    redirect_to cart_path
+  end
+
   def index
   end
 end
--- spec/features/user_can_add_item_to_cart_spec.rb
+++ spec/features/user_can_add_item_to_cart_spec.rb
@@ -3,11 +3,12 @@ require "rails_helper"
 RSpec.feature "user can add item to cart" do
   it "has the item in the cart" do
     visit items_path
+
     click_on("Add Fries")
 
     visit cart_path
 
-    expect(page).to have_content("Fries: 1")
+    expect(page).to have_content("Fries 1")
     expect(current_path).to eq(cart_path)
   end
 end
