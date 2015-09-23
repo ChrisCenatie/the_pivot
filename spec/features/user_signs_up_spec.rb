ca7af20d183d87c3042b8ae3e2166c0eaffdabd4
David Stinnette <dastinnette@gmail.com>
2015-09-22 19:42:13 -0600

    Add functionality where new user may sign up from root

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -4,7 +4,12 @@ RSpec.feature "user signs up" do
   let(:user) { User.find_by(email: "Justin@example.com") }
 
   scenario "with correct credentials" do
-    visit new_user_path
+    visit root_path
+    click_link("Sign up")
+
+    expect(page).to have_content("Sign up")
+    expect(current_path).to eq(new_user_path)
+
     fill_in "Email", with: "Justin@example.com"
     fill_in "Password", with: "password"
     click_button "Create Account"
