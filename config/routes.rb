e30dc3098bd1a2c5765d80cb99ba86ca61ee0f9a
Michael Wong <michael.brian.wong@gmail.com>
2015-09-26 14:23:15 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   get "login",        to: "sessions#new"
   post "login",       to: "sessions#create"
   delete "logout",    to: "sessions#destroy"
-  get "dashboard",    to: "sessions#new"
 
   resources :orders, only: [:create, :show, :index]
 
