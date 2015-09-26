a5633d64723a9bb9556174e690b18ab19a9891b4
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-26 14:38:55 -0600

    add tests for viewing item from various pages

--- spec/features/user_can_view_item_show_spec.rb
+++ spec/features/user_can_view_item_show_spec.rb
@@ -1,9 +1,47 @@
 require 'rails_helper'
 
-describe 'the item show', type: :feature do
-  let(:item) { Item.create(name: "Soda", description: "Teh bubbles", price:100) }
-  it 'can show one item' do
-    visit "/items/#{item.id}"
+describe 'can view an item show page', type: :feature do
+  let(:item) { item.create(name: "soda", description: "teh bubbles", price:100) }
+
+  before(:each) do
+   Item.create(name: "Soda", description: "Teh bubbles", price:100) 
+   User.create(email: "david@example.com", password: "password")
+  end
+
+
+  scenario 'from the item index' do
+    visit items_path
+    click_on "Soda" 
+
+    expect(page).to have_content('Soda')
+    expect(page).to have_content('Teh bubbles')
+    expect(page).to have_content('100')
+  end
+
+  scenario 'from the cart' do
+    visit items_path
+    click_on "Add Soda" 
+    click_on "Cart" 
+    click_on "Soda"
+
+    expect(page).to have_content('Soda')
+    expect(page).to have_content('Teh bubbles')
+    expect(page).to have_content('100')
+  end
+
+  scenario 'from past orders' do
+    visit items_path
+    click_on "Add Soda" 
+    click_on "Cart"
+    click_on "Check Out" 
+
+    fill_in "Email", with: "david@example.com"
+    fill_in "Password", with: "password"
+
+    click_button("Login")
+    click_button("Check Out")
+    click_on "Soda" 
+
     expect(page).to have_content('Soda')
     expect(page).to have_content('Teh bubbles')
     expect(page).to have_content('100')
