f2f1ed0bf191a4ca9ec891b9390a4862c315e354
Justin Holzmann <jpholzmann@gmail.com>
2015-09-26 20:19:32 -0600

    Update user_can_view_item_index_spec.rb

--- spec/features/user_can_view_item_index_spec.rb
+++ spec/features/user_can_view_item_index_spec.rb
@@ -5,7 +5,6 @@ describe 'the item view', type: :feature do
   it 'shows the items' do
     Item.create(name: 'Fries', description: 'Fo Free', price: 4)
     visit root_path
-
     visit items_path
 
     expect(page).to have_content('Fries')
