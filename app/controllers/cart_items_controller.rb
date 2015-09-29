b23b348b74f078db8614c873c5ed1f5746146761
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-29 14:23:57 -0600

    refactor cart_items_controller

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,25 +1,44 @@
 class CartItemsController < ApplicationController
   def create
-    cart.add_item(params[:item_id])
+    cart.add_item(item.id)
     session[:cart] = cart.data
-    flash[:notice] = "Successfully added #{Item.find(params[:item_id]).name} to cart"
-    redirect_to category_items_path(Category.find(params[:category_id]))
+    flash[:notice] = "Successfully added #{item.name} to cart"
+    redirect_to category_items_path(category)
   end
 
   def update
-    cart.add_item(params[:item_id])
+    cart.add_item(item.id)
     session[:cart] = cart.data
-    flash[:notice] = "Successfully added #{Item.find(params[:item_id]).name} to cart"
-    redirect_to cart_path
+    flash[:notice] = "Successfully added #{item.name} to cart"
+    redirect_to redirect_path 
   end
 
   def destroy
-    cart.remove_item(params[:item_id])
+    cart.remove_item(item.id)
     session[:cart] = cart.data
-    flash[:notice] = "Successfully removed #{Item.find(params[:item_id]).name} from cart"
+    flash[:notice] = "Successfully removed #{item.name} from cart"
     redirect_to cart_path
   end
 
   def index
   end
+
+  private
+
+  def redirect_path
+    if previous_path == category_item_path(item.category, item)
+       previous_path 
+    else
+      cart_path
+    end
+  end
+
+  def item
+    Item.find(params[:item_id])
+  end
+
+  def category
+    Category.find(params[:category_id])
+  end
+
 end
