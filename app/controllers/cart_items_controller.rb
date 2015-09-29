7f0ca924db95883fcb4792bea8e335c46fcbd5e5
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-28 18:25:40 -0600

    adds messages for editing item counts fom cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,12 +9,14 @@ class CartItemsController < ApplicationController
   def update
     cart.add_item(params[:item_id])
     session[:cart] = cart.data
+    flash[:notice] = "Successfully added #{Item.find(params[:item_id]).name} to cart"
     redirect_to cart_path
   end
 
   def destroy
     cart.remove_item(params[:item_id])
     session[:cart] = cart.data
+    flash[:notice] = "Successfully removed #{Item.find(params[:item_id]).name} to cart"
     redirect_to cart_path
   end
 
--- spec/features/user_can_add_item_to_cart_spec.rb
+++ spec/features/user_can_add_item_to_cart_spec.rb
@@ -23,6 +23,7 @@ RSpec.feature "user can add item to cart" do
     visit items_path
 
     click_on("Add Fries")
+    expect(page).to have_content("Successfully added Fries")
     click_on("Add Burger")
     click_on("Cart")
     click_on("Add Fries")
