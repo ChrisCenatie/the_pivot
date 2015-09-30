63b2006233f25a1f2d80a63102806a8a79d57d67
philmphoenix <jpholzmann@gmail.com>
2015-09-30 12:23:07 -0600

    Fixed bugs on homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
 
   def index
-   @disable_nav = true 
+   @disable_nav = true
   end
   def show
     vendor_data = [ { name: "McDonalds", image_url: "http://www.walnet.ca/sites/default/files/styles/portfolio-thumbnail/public/portfolio/mcdonalds.jpg?itok=nC_5t4cQ"},
@@ -9,7 +9,7 @@ class StaticController < ApplicationController
       {name: "TGI Friday's", image_url: "http://www.microsoftproductreviews.com/wp-content/uploads/2015/01/Video-Production-Birmingham-London-TGI-Fridays-Promo-Video-13.png"} ]
 
     @vendors = vendor_data.map { |v| Vendor.new(v) }
-    
+
     render :vendors_index
   end
 end
