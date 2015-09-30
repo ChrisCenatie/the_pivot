511678d04ff01277fb5665e3cc1a06038412973c
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-29 22:00:00 -0600

    removes navbar from splash

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,8 @@
 class StaticController < ApplicationController
 
+  def index
+   @disable_nav = true 
+  end
   def show
     vendor_data = [ { name: "McDonalds", image_url: "http://www.walnet.ca/sites/default/files/styles/portfolio-thumbnail/public/portfolio/mcdonalds.jpg?itok=nC_5t4cQ"},
       {name: "Hapa Sushi", image_url: "http://denver.cities2night.com/public/place_images/place_4156.jpg"},
