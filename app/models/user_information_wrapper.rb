eecf9f5c5f909d8483b239c5a7c67c8ad008931d
philmphoenix <jpholzmann@gmail.com>
2015-10-01 07:35:51 -0600

    Fixes spacing in information wrapper poro

--- app/models/user_information_wrapper.rb
+++ app/models/user_information_wrapper.rb
@@ -6,19 +6,19 @@ class UserInformationWrapper
   end
 
   def user_params
-    { email: params[:email],
-      password: params[:password],
+    { email:        params[:email],
+      password:     params[:password],
       phone_number: params[:phone_number],
-      first_name: params[:first_name],
-      last_name: params[:last_name] }.select {|_,v| v }.to_h
+      first_name:   params[:first_name],
+      last_name:    params[:last_name] }.select {|_,v| v }.to_h
   end
 
   def address_params
     { street_address: params[:street_address],
-      apt: params[:apt],
-      city: params[:city],
-      state: params[:state],
-      zip_code: params[:zip_code],
-      user_id: params[:user_id] }.select {|_,v| v }.to_h
+      apt:            params[:apt],
+      city:           params[:city],
+      state:          params[:state],
+      zip_code:       params[:zip_code],
+      user_id:        params[:user_id] }.select {|_,v| v }.to_h
   end
 end
