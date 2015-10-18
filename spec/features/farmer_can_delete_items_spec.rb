6b4149947a3bac378b309b860fdab7d75aff16ce
David Stinnette <dastinnette@gmail.com>
2015-10-18 15:53:55 -0600

    Farmer admin cannot edit another farmer's items

---
+++ spec/features/farmer_can_delete_items_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "farmer admin can" do
+
+  scenario "edit items with valid params" do
+    create_item!
+    login_admin!
+    visit category_items_path(category)
+
+    expect(page).to have_content("Delete")
+    click_on("Delete")
+
+    expect(current_path).to eq(category_items_path(category))
+
+    expect(page).to_not have_content("Fries")
+    expect(page).to_not have_content("Yummy")
+  end
+end
--- spec/features/farmer_can_edit_items_spec.rb
+++ spec/features/farmer_can_edit_items_spec.rb
@@ -21,7 +21,7 @@ RSpec.feature "farmer admin can" do
     fill_in("Price", with: "15.00")
     fill_in("Image Url", with: "http://www.reservoirhillsfamilybutchery.co.za/wp-content/uploads/2015/03/RoastChicken.jpg")
     click_on("Update Item")
-    
+
     expect(current_path).to eq(category_items_path(category))
     expect(page).to have_content("Roast Beef")
     expect(page).to have_content("Pa's")
@@ -39,5 +39,15 @@ RSpec.feature "farmer admin can" do
 
     expect(current_path).to eq(edit_farmer_admin_item_path(Item.last))
     expect(page).to have_content("Invalid update params")
+  end
+
+  scenario "not edit item from another farmer" do
+    create_item1!
+
+    visit category_items_path(category)
+
+    within("#item#{item1.id}") do
+      expect(page).to_not have_content("Edit")
+    end
   end
 end
