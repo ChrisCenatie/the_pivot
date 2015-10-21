e9b41cffea01066796a5c93d05fe5e0dae19da89
George Hudson <georgehudson78@gmail.com>
2015-10-21 10:48:32 -0600

    refactored non farmer admin permissions test

--- spec/features/non_farmer_admin_cannot_access_farmer_admin_pages_spec.rb
+++ spec/features/non_farmer_admin_cannot_access_farmer_admin_pages_spec.rb
@@ -2,33 +2,27 @@ require "rails_helper"
 
 RSpec.feature "non farmer admin cannot access farmer admin pages" do
 
-  scenario "as guest" do
+  def cannot_visit_farmer_admin_pages
     visit "/farmer_admin/dashboard"
     page.status_code.should eq(404)
     visit "/farmer_admin/users/new"
     page.status_code.should eq(404)
     visit "/farmer_admin/farmers/1/edit"
     page.status_code.should eq(404)
+  end
+
+  scenario "as guest" do
+    cannot_visit_farmer_admin_pages
   end
 
   scenario "as registered user" do
     login_user!
-    visit "/farmer_admin/dashboard"
-    page.status_code.should eq(404)
-    visit "/farmer_admin/users/new"
-    page.status_code.should eq(404)
-    visit "/farmer_admin/farmers/1/edit"
-    page.status_code.should eq(404)
+    cannot_visit_farmer_admin_pages
   end
 
   scenario "as registered admin" do
     login_admin!
-    visit "/farmer_admin/dashboard"
-    page.status_code.should eq(404)
-    visit "/farmer_admin/users/new"
-    page.status_code.should eq(404)
-    visit "/farmer_admin/farmers/1/edit"
-    page.status_code.should eq(404)
+    cannot_visit_farmer_admin_pages
   end
 
 
