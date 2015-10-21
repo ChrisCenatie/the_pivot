bae59f9191b4466987290d09c62a88fd0f665ddb
George Hudson <georgehudson78@gmail.com>
2015-10-21 13:22:28 -0600

    Refactored address creation and update spec. Also refactored previous path method to allow for address creation errors and still get back to previous page

--- spec/features/user_can_enter_address_in_dashboard_spec.rb
+++ spec/features/user_can_enter_address_in_dashboard_spec.rb
@@ -21,11 +21,66 @@ RSpec.feature "user can enter address in dashboard" do
 
     click_on("Hello, #{user.name}")
     click_on("Edit Profile")
+
+    expect(page).to have_link("Back to Profile")
+
+    click_on("Back to Profile")
+
+    expect(current_path).to eq(user_path(user))
+
+    click_on("Edit Profile")
     enter_name
+
+    expect(page).to have_content("Hello, Josh")
+    expect(current_path).to eq(edit_user_path(user.id))
+
     click_on("Add Address")
+
+    expect(current_path).to eq(new_address_path)
+
     enter_address
 
     expect(current_path).to eq(edit_user_path(user.id))
     expect(page).to have_content("Address successfully added")
+
+    fill_in("address[street_address]", with: "1515 Blake Street")
+    fill_in("address[city]", with: "Denver")
+    fill_in("address[state]", with: "CO")
+    fill_in("address[zip_code]", with: "80012")
+    click_on("Update Address")
+
+    expect(current_path).to eq(edit_user_path(user.id))
+    expect(page).to have_content("Address successfully updated")
+  end
+
+  scenario "with missing parameters" do
+    login_user!
+
+    click_on("Hello, #{user.name}")
+    click_on("Edit Profile")
+    fill_in("user_and_address[first_name]", with: "Josh")
+    click_on("Update Name")
+
+    expect(current_path).to eq(edit_user_path(user.id))
+    expect(page).to have_content("Invalid input params")
+    expect(page).to_not have_content("Hello, Josh")
+
+    click_on("Add Address")
+    fill_in("City", with: "Denver")
+    fill_in("State", with: "CO")
+    fill_in("Zip Code", with: "80010")
+    click_on("Update")
+
+    expect(current_path).to eq(new_address_path)
+    expect(page).to have_content("Street address can't be blank")
+
+    enter_address
+
+    expect(current_path).to eq(edit_user_path(user.id))
+    fill_in("address[state]", with: "Colorado")
+    click_on("Update Address")
+
+    expect(current_path).to eq(edit_user_path(user.id))
+    expect(page).to have_content("Invalid Address Attributes")
   end
 end
