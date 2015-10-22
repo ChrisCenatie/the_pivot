b150a02b23de7985315142755d8e09bd409ea17d
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-10-22 11:44:51 -0600

    Code has been refactored

--- app/controllers/farmer_admin/orders_controller.rb
+++ app/controllers/farmer_admin/orders_controller.rb
@@ -2,13 +2,9 @@ class FarmerAdmin::OrdersController < FarmerAdminController
 
   def update
     order = Order.find(order_params[:id])
-    order_items = order.order_items
     farmer = current_user.farmer
-    order_items.each do |order_item|
-      order_item.delete if farmer.items.include?(order_item.item)
-    end
-    FarmerOrder.find_by(order_id: order_params[:id],
-                        farmer_id: farmer.id).delete
+    FarmerAdminOrders.remove_order_items(order, farmer)
+    FarmerAdminOrders.remove_farmer_order(farmer, order_params)
     flash[:notice] = "Order has been cancelled!"
     redirect_to farmer_admin_dashboard_path
   end
--- app/controllers/farmer_admin/users_controller.rb
+++ app/controllers/farmer_admin/users_controller.rb
@@ -2,15 +2,7 @@ class FarmerAdmin::UsersController < FarmerAdminController
   def show
     @farmer_admins = User.where(farmer: current_user.farmer.id)
     farmer = current_user.farmer
-    if !farmer.orders.empty?
-      orders = farmer.orders.all.order(:id)
-      order_data = orders.map do |order|
-        [order, [order_total(order), items(order)]]
-      end
-      @orders_data = order_data.to_h
-    else
-      @orders_data = nil
-    end
+    @orders_data = FarmerAdminOrders.farmer_order_data(farmer)
   end
 
   def update
@@ -32,9 +24,7 @@ class FarmerAdmin::UsersController < FarmerAdminController
   def create
     user = User.new(user_params)
     if user.valid?
-      user.role = 2
-      user.farmer_id = current_user.farmer.id
-      user.save
+      create_farmer_admin(user)
       flash[:notice] = "New Farmer Admin, #{user.name} Added for #{current_user.farmer.name}!"
       redirect_to farmer_admin_dashboard_path
     else
@@ -45,30 +35,14 @@ class FarmerAdmin::UsersController < FarmerAdminController
 
   private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :phone_number, :first_name,
-      :last_name)
-  end
-
-  def order_total(order)
-    order_items = order.order_items
-    price_quantities = order_items.map do |order_item|
-      if current_user.farmer.items.include?(Item.find(order_item.item_id))
-        order_item.quantity * order_item.price
-      else
-        0
-      end
+    def user_params
+      params.require(:user).permit(:email, :password, :phone_number, :first_name,
+        :last_name)
     end
-    price_quantities.reduce(:+)
-  end
 
-  def items(order)
-    order_items = order.order_items
-    items = []
-    order_items.each do |order_item|
-      item = Item.find(order_item.item_id)
-      items << item.name if current_user.farmer.items.include?(item)
+    def create_farmer_admin(user)
+      user.role = 2
+      user.farmer_id = current_user.farmer.id
+      user.save
     end
-    items
-  end
 end
